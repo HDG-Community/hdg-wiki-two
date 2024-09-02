cad6ab3885c00dc8696ea57cb9f28d2447b6d007
anbl <me@anbl.cc>
2024-09-02 15:15:40 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "An experimental wiki for the hdg community";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    inputs.utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        inherit (pkgs) lib;
+      in {
+        devShells.default = let
+          name = "hdg-wiki";
+        in pkgs.mkShell {
+          inherit name;
+
+          packages = [
+            pkgs.hugo
+          ];
+        };
+      });
+}
